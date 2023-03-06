40a7287e37753714c098d80cc8d6e622f183a0e0
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-06 01:40:35 +0000

    Added AudioReactive

---
+++ java/src/ie/tudublin/Visuals/AudioReactive.java
@@ -0,0 +1,20 @@
+package ie.tudublin.Visuals;
+
+import ie.tudublin.*;
+
+/**
+ * AudioReactive is an abstract class which will be used to create
+ * objects within the Music Visualiser which are audio reactive
+ *
+ * @version 1.0
+ * @since 06-03-2023
+ * @author AdrianCapacite
+ */
+public abstract class AudioReactive {
+    Visual visual;
+    public AudioReactive(Visual visual) {
+        this.visual = visual;
+    }
+    public abstract void render();
+
+}
