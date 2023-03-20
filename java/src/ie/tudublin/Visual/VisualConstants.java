384194204c54d2eeb34cfc4e862b8d637439f2c0
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-20 07:48:07 +0000

    Added VisualConstant

---
+++ java/src/ie/tudublin/Visual/VisualConstants.java
@@ -0,0 +1,10 @@
+package ie.tudublin.Visual;
+
+public interface VisualConstants {
+    public static final int MIX = 0;
+    public static final int LEFT = 1;
+    public static final int RIGHT = 2;
+
+    public static final int LINEIN = 1;
+    public static final int AUDIOFILE = 0;
+}
