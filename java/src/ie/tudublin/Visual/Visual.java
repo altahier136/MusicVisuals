2afd3ebd27dfa460e7f82d6e4ac806efcd238fc5
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-21 12:27:12 +0000

    Restructured Visual and added Ease

--- java/src/ie/tudublin/Visual/Visual.java
+++ java/src/ie/tudublin/Visual/Visual.java
@@ -1,15 +1,21 @@
 package ie.tudublin.Visual;
 
 import processing.core.PApplet;
+
+import java.lang.reflect.Method;
+
 import ddf.minim.*;
 import ddf.minim.analysis.*;
 
 /**
- * <p>Visual is the main class which will be used to create the Music Visualiser
+ * <p>
+ * Visual is the main class which will be used to create the Music Visualiser
  * The {@link Visual} class is an abstract class that will be used to create
- * the Music Visualiser.</p>
+ * the Music Visualiser.
+ * </p>
  *
- * <p>Getters and setters:
+ * <p>
+ * Getters and setters:
  * <ul>
  * <li>{@link #getFrameSize}</li>
  * <li>{@link #setFrameSize}</li>
@@ -23,97 +29,34 @@ import ddf.minim.analysis.*;
  * <li>{@link #getAudioBuffer}</li>
  *
  */
-public abstract class Visual extends PApplet
-{
-    private int frameSize = 512;
-    private int sampleRate = 44100;
+public abstract class Visual extends PApplet implements VisualConstants {
+    private int bufferSize;
+    private int sampleRate;
 
-    private float[] bands;
-    private float[] smoothedBands;
+    private float[] bands, lerpedBands;
+    private float amplitude, lerpedAmplitude;
 
     private Minim minim;
     private AudioInput ai;
     private AudioPlayer ap;
-    private AudioBuffer abMono;
+    private AudioBuffer abMix;
     private AudioBuffer abLeft;
     private AudioBuffer abRight;
     private FFT fft;
-
-    private float amplitude  = 0;
-    private float smothedAmplitude = 0;
-
-    enum AudioChannel {
-        MONO, LEFT, RIGHT
-    }
-
-    /**
-     * Sets minim, fft, bands and smoothedBands
-     */
-    public void startMinim()
-    {
-        minim = new Minim(this);
-
-        fft = new FFT(frameSize, sampleRate);
-
-        bands = new float[(int) log2(frameSize)];
-          smoothedBands = new float[bands.length];
-    }
-
-    /**
-     * Calculates the average amplitude of the audio buffer
-     */
-    public void calculateAverageAmplitude()
-    {
-        float total = 0;
-        for(int i = 0 ; i < abMono.size() ; i ++)
-        {
-            total += abs(abMono.get(i));
-        }
-        amplitude = total / abMono.size();
-        smothedAmplitude = PApplet.lerp(smothedAmplitude, amplitude, 0.1f);
-    }
-
-    /**
-     * Starts listening to mic input
-     */
-    public void startListening()
-    {
-        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
-        abLeft = ai.left;
-        abRight = ai.right;
-        abMono = ai.mix;
-    }
-
-    /**
-     * Loads an audio file
-     * @param filename
-     */
-    public void loadAudio(String filename)
-    {
-        ap = minim.loadFile(filename, frameSize);
-        abLeft = ap.left;
-        abRight = ap.right;
-        abMono = ap.mix;
-    }
+    private BeatDetect beat;
 
     /**
      * Gets the frame size.
-     * @return {@link #frameSize}
+     *
+     * @return {@link #bufferSize}
      */
-    public int getFrameSize() {
-        return frameSize;
-    }
-
-    /**
-     * Sets the {@link #frameSize}
-     * @param frameSize
-     */
-    public void setFrameSize(int frameSize) {
-        this.frameSize = frameSize;
+    public int getBufferSize() {
+        return bufferSize;
     }
 
     /**
      * Gets the sample rate.
+     *
      * @return {@link #sampleRate}
      */
     public int getSampleRate() {
@@ -121,15 +64,8 @@ public abstract class Visual extends PApplet
     }
 
     /**
-     * Sets the {@link #sampleRate}
-     * @param sampleRate
-     */
-    public void setSampleRate(int sampleRate) {
-        this.sampleRate = sampleRate;
-    }
-
-    /**
      * Gets the bands.
+     *
      * @return {@link #bands}
      */
     public float[] getBands() {
@@ -138,14 +74,16 @@ public abstract class Visual extends PApplet
 
     /**
      * Gets the smoothed bands.
-     * @return {@link #smoothedBands}
+     *
+     * @return {@link #lerpedBands}
      */
-    public float[] getSmoothedBands() {
-        return smoothedBands;
+    public float[] getLerpedBands() {
+        return lerpedBands;
     }
 
     /**
      * Gets the Minim object.
+     *
      * @return {@link #minim}
      */
     public Minim getMinim() {
@@ -154,6 +92,7 @@ public abstract class Visual extends PApplet
 
     /**
      * Gets the AudioPlayer object.
+     *
      * @return {@link #ap}
      */
     public AudioInput getAudioInput() {
@@ -161,102 +100,237 @@ public abstract class Visual extends PApplet
     }
 
     /**
-     * Gets the {@link #abMono} object if the parameter is {@link #MONO}. <br>
-     * Gets the {@link #abLeft} object if the parameter is {@link #LEFT}. <br>
-     * Gets the {@link #abRight} object if the parameter is {@link #RIGHT}. <br>
-     * @param mode
+     * Gets the AudioPlayer object.
+     *
+     * @param channel 0 for {@link #abMix}, 1 for {@link #abLeft}, 2 for
+     *                {@link #abRight}
      * @return abMono
      */
-    public AudioBuffer getAudioBuffer(AudioChannel mode) {
-        switch (mode) {
-        case MONO:
-            return abMono;
-        case LEFT:
-            return abLeft;
-        case RIGHT:
-            return abRight;
-        default:
-            return abMono;
+    public AudioBuffer getAudioBuffer(int channel) {
+        switch (channel) {
+            case VisualConstants.MIX:
+                return abMix;
+            case VisualConstants.LEFT:
+                return abLeft;
+            case VisualConstants.RIGHT:
+                return abRight;
+            default:
+                return abMix;
         }
     }
-    /**
-     * Gets {@link #abMono}
-     * @return abMono
-     */
-    public AudioBuffer getAudioBuffer() {
-        return abMono;
-    }
-    /**
-     * Gets the amplitude.
-     * @return {@link #amplitude}
-     */
+
+    /** Gets the amplitude. */
     public float getAmplitude() {
         return amplitude;
     }
-    /**
-     * Gets the smoothed amplitude.
-     * @return {@link #smothedAmplitude}
-     */
-    public float getSmoothedAmplitude() {
-        return smothedAmplitude;
-    }
 
     /**
-     * Gets the AudioPlayer object.
-     * @return {@plain #ap}
+     * Gets the amplitude.
+     *
+     * @param mode 0 for {@link #amplitude} and 1 for {@link #lerpedAmplitude}
      */
+    public float getAmplitude(int mode) {
+        if (mode == 0)
+            return amplitude;
+        else
+            return lerpedAmplitude;
+    }
+
+    /** Gets the AudioPlayer object. */
     public AudioPlayer getAudioPlayer() {
         return ap;
     }
 
-    /**
-     * Gets the FFT object.
-     * @return {@link #fft}
-     */
+    /** Gets the FFT object. */
     public FFT getFFT() {
         return fft;
     }
 
+    public Visual() throws VisualException {
+        // Default buffer size and sample rate
+        this(1024, 44100);
+    }
+
+    public Visual(int bufferSize, int sampleRate) throws VisualException {
+
+        if (log2(bufferSize) % 1 != 0)
+            throw new VisualException("Buffer size must be a power of 2 for FFT analysis");
+        this.bufferSize = bufferSize;
+        this.sampleRate = sampleRate;
+
+        // Audio analysis
+        minim = new Minim(this);
+        fft = new FFT(bufferSize, sampleRate);
+        beat = new BeatDetect(bufferSize, sampleRate);
+        beat.setSensitivity(100);
+
+        bands = new float[(int) log2(bufferSize)];
+        lerpedBands = new float[bands.length];
+
+        amplitude = 0;
+        lerpedAmplitude = 0;
+    }
+
+    abstract public void settings();
+    abstract public void setup();
+    abstract public void draw();
+
+    // ======== Audio Analysis ========
+
     /**
-     * Calculates the FFT of the audio buffer
+     * Begins audio input from the default audio input device.
+     *
+     * @throws VisualException
      */
-    protected void calculateFFT() throws VisualException
-    {
-        fft.window(FFT.HAMMING);
-        if (abMono != null)
-        {
-            fft.forward(abMono);
+    public void beginAudio() throws VisualException {
+        if (ap != null)
+            ap.close();
+        ai = minim.getLineIn(Minim.MONO, bufferSize, 44100, 16);
+        abLeft = ai.left;
+        abRight = ai.right;
+        abMix = ai.mix;
+    }
+
+    /**
+     * Begins audio input from the specified audio file.
+     *
+     * @param filename
+     * @throws VisualException
+     */
+    public void beginAudio(String filename) throws VisualException {
+        if (ap != null)
+            ap.close();
+        if (filename == null || filename.isEmpty()) {
+            System.out.println("No filename specified, using default audio input");
+            beginAudio();
         }
-        else
-        {
+        ap = minim.loadFile(filename, bufferSize);
+        abLeft = ap.left;
+        abRight = ap.right;
+        abMix = ap.mix;
+    }
+
+    // ======== Waveform Analysis ========
+
+    /**
+     * Calculates the average amplitude of the audio buffer
+     *
+     * @param amt The amount to lerp the amplitude by (0.0 - 1.0) recommended 0.1
+     */
+    public void lerpAmplitude(float amt) {
+        // Get the average amplitude of the audio buffer
+        float sum = 0;
+        for (float sample : abMix.toArray()) {
+            sum += abs(sample);
+        }
+        amplitude = sum / abMix.size();
+
+        // Lerp the amplitude
+        // lerpedAmplitude = PApplet.lerp(lerpedAmplitude, amplitude, amt);
+        lerpedAmplitude = mlerp(lerpedAmplitude, amplitude, amt, 1 / frameRate);
+    }
+
+    // ======== Fast Fourier Analysis ========
+
+    /** Calculates the FFT of the audio buffer */
+    protected void calculateFFT() throws VisualException {
+        fft.window(FFT.HAMMING);
+        if (abMix != null) {
+            fft.forward(abMix);
+        } else {
             throw new VisualException("You must call startListening or loadAudio before calling fft");
         }
     }
 
     /**
      * Calculates the frequency bands of the audio buffer
+     *
+     * @param amt The amount to lerp the bands by (0.0 - 1.0) recommended 0.05
      */
-    protected void calculateFrequencyBands() {
+    protected void calculateFrequencyBands(float amt) {
         for (int i = 0; i < bands.length; i++) {
-            int start = (int) pow(2, i) - 1;
-            int w = (int) pow(2, i);
-            int end = start + w;
+            int start = (int) pow(2, i) - 1; // start at 0
+            int w = (int) pow(2, i); // width of each band
+            int end = start + w; // end at the last index
+            end = min(end, fft.specSize() - 1); // make sure it doesn't go out of bounds
+
             float average = 0;
+            // For each band, add the value of each bin
+
             for (int j = start; j < end; j++) {
                 average += fft.getBand(j) * (j + 1);
             }
             average /= (float) w;
             bands[i] = average * 5.0f;
-            smoothedBands[i] = lerp(smoothedBands[i], bands[i], 0.05f);
+            lerpedBands[i] = lerp(lerpedBands[i], bands[i], 0.05f);
         }
     }
 
-    /**
-     * Log_2(x) = log_e(x) / log_e(2)
-     */
+    // ======== Beat Detection ========
+
+    // ======== Helpers ========
+
+    /** Log_2(x) = log_e(x) / log_e(2) */
     float log2(float f) {
         return log(f) / log(2.0f);
     }
 
+    /*
+     * LERP (Linear Interpolation)
+     * Issue with base lerp is it has different behaviors at different frame
+     * rates, below is the different methods of lerp for a moving target
+     * Method 1: (not frame rate independent)
+     * K = constant (such as 0.1)
+     * pos = lerp(pos, target, K)
+     *
+     * Method 2: (frame rate independent but not smooth)
+     * K = constant * frameTime
+     * pos = lerp(pos, target, K)
+     *
+     * Method 3: (frame rate independent and smooth)
+     * K = constant - (K * frameTime)
+     * pos = lerp(pos, target, K)
+     *
+     * See: https://youtu.be/YJB1QnEmlTs
+     */
+    /**
+     * Implementation of lerp that is frame rate independent and smooth for
+     * a moving target
+     *
+     * @param start
+     * @param stop
+     * @param amt
+     * @param frameTime
+     * @return
+     */
+    public float mlerp(float start, float stop, float amt, float frameTime) {
+        float K = amt - (amt * frameTime);
+        return start + (stop - start) * K;
+    }
+
+    interface EaseInterface {
+        float ease(float t);
+    }
+
+    EaseInterface easelinear = (t) -> t;
+    EaseInterface easeSmooth = (t) -> t * t * (3 - 2 * t);
+    EaseInterface easeInQuad = (t) -> t * t;
+    EaseInterface easeOutQuad = (t) -> t * (2 - t);
+    EaseInterface easeInOutQuad = (t) -> t < 0.5 ? 2 * t * t : 1 - pow(-2 * t + 2, 2) / 2;
+    EaseInterface easeOutBounce = (t) -> {
+        final float n1 = 7.5625f;
+        final float d1 = 2.75f;
+
+        if (t < 1 / d1)
+            return n1 * t * t;
+
+        if (t < 2 / d1)
+            return n1 * (t -= 1.5f / d1) * t + 0.75f;
+
+        if (t < 2.5 / d1)
+            return n1 * (t -= 2.25f / d1) * t + 0.9375f;
+
+        return n1 * (t -= 2.625f / d1) * t + 0.984375f;
+    };
 
 }
