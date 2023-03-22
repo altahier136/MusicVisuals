ea9148321837cf9bd09d8ec75223e56da8970af4
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-22 14:52:32 +0000

    Implemented Object

--- java/src/ie/tudublin/Visual/Object.java
+++ java/src/ie/tudublin/Visual/Object.java
@@ -1,5 +1,6 @@
 package ie.tudublin.Visual;
 
+import processing.core.PApplet;
 import processing.core.PVector;
 
 public abstract class Object {
@@ -7,6 +8,20 @@ public abstract class Object {
     private Visual v;
     public PVector position;
     public PVector rotation;
+    public PVector scale;
+
+    public Visual visual() {
+        return v;
+    }
+    public PVector position() {
+        return position;
+    }
+    public PVector rotation() {
+        return rotation;
+    }
+    public PVector scale() {
+        return scale;
+    }
 
     Object(Visual v) {
         this(v, new PVector(0,0,0), new PVector(0,0,0));
@@ -20,6 +35,22 @@ public abstract class Object {
         this.rotation = rotation;
     }
 
-    abstract void render();
+    public void render() {
+        applyTransforms();
+        v.colorMode(PApplet.RGB);
+        v.fill(255, 0, 255);
+        v.circle(10, 10, 10);
+        v.popMatrix();
+        System.out.println(this.getClass().getName() + "Warning: Empty Render Method");
+    }
+
+    public void applyTransforms() {
+        v.pushMatrix();
+        v.translate(position.x, position.y, position.z);
+        v.rotateX(rotation.x);
+        v.rotateY(rotation.y);
+        v.rotateZ(rotation.z);
+        v.scale(scale.x, scale.y, scale.z);
+    }
 
 }
