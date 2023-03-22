ab5548d8ca3b77ea12b1964998d957f5a3495bae
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-22 14:48:58 +0000

    Added ChannelEnum

--- java/src/ie/tudublin/Visual/VisualConstants.java
+++ java/src/ie/tudublin/Visual/VisualConstants.java
@@ -1,10 +1,7 @@
 package ie.tudublin.Visual;
 
 public interface VisualConstants {
-    public static final int MIX = 0;
-    public static final int LEFT = 1;
-    public static final int RIGHT = 2;
-
-    public static final int LINEIN = 1;
-    public static final int AUDIOFILE = 0;
+    enum ChannelEnum {
+        MIX, LEFT, RIGHT
+    }
 }
