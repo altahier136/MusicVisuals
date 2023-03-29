6a9e8310bdeee40ba48b0718cac8e591acd42fb3
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-03-29 13:28:44 +0100

    Fixed class naming

---
+++ java/src/ie/tudublin/Visual/VConstants.java
@@ -0,0 +1,13 @@
+package ie.tudublin.Visual;
+
+/**
+ * The VisualConstants interface is used to store constants that are used in the
+ * Visual class and its related classes.
+ *
+ * @see {@link ie.tudublin.Visual.Visual}
+ */
+public interface VConstants {
+    enum ChannelEnum {
+        MIX, LEFT, RIGHT
+    }
+}
