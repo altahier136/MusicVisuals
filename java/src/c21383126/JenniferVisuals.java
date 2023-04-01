af6f0b49a0049ce88bffdf0d9240a8a79df7aff0
JKearns3 <C21383126@mytudublin.ie>
2023-04-01 13:15:21 +0100

    Tried changing to the beat, not fully working

--- java/src/c21383126/JenniferVisuals.java
+++ java/src/c21383126/JenniferVisuals.java
@@ -20,7 +20,8 @@ public class JenniferVisuals extends VScene {
     public void render(int elapsed) {
       
         // 1:03 - 1:48 - Second verse & chorus
-        if (elapsed > v.toMs(1, 3, 0) && elapsed < v.toMs(1, 48, 0)) {
+        if (elapsed > v.toMs(1, 3, 25) && elapsed < v.toMs(1, 48, 0)) //start at 1:03:something so the beats match up
+        {
             speaker.render();                  
         }
         System.out.println(elapsed);
@@ -37,12 +38,9 @@ public class JenniferVisuals extends VScene {
             v.colorMode(PApplet.HSB, 360, 100, 100);
             v.background(360);
             v.stroke(255);
-            int radius = 150;
-            int border = radius + 10;
 
             // (x1,y1) (x3,y3)
             // (x2,y2) (x4,y4)
-
             
             int x1 = v.width/4;
             int y1 = v.height/4;
@@ -52,16 +50,19 @@ public class JenniferVisuals extends VScene {
             int y3 = v.height/4;
             int x4 = v.width/4 * 3;
             int y4 = v.height/4 * 3;
-            
+
+            int radius = 200;
+            int border = radius + 5;            
 
             int length = ((y2 + border) - (y1 - border));
+            int width = ((x2 + border) - (x1 - border));
             v.fill(100);
-            v.rect(x1 - border, y1 - border, 2*border, length);
+            v.rect(x1 - border, y1 - border, width, length);
 
-            v.rect(x3 - border, y3 - border, 2*border, length);
+            v.rect(x3 - border, y3 - border, width, length);
 
             v.noStroke();
-            v.frameRate(1);
+            v.frameRate((float)0.625); //1/96 of a second bc 96bpm
             
             float h = v.random(0, 360);
             for (int r = radius; r > 0; --r) {
