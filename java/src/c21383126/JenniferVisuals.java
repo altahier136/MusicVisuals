be31327d04548b81bd1c8336d78b413ec6ee09ba
JKearns3 <C21383126@mytudublin.ie>
2023-04-12 16:10:57 +0100

    Speakers reacting to music and added circle lines thing from lab 5

--- java/src/c21383126/JenniferVisuals.java
+++ java/src/c21383126/JenniferVisuals.java
@@ -6,15 +6,30 @@ import ie.tudublin.visual.Visual;
 //import ie.tudublin.visual.VConstants.ChannelEnum;
 import processing.core.PApplet;
 import processing.core.PVector;
+import ddf.minim.AudioBuffer;
+import ddf.minim.Minim;
+
+
 
 public class JenniferVisuals extends VScene {
     Visual v;
     VObject speaker;
+    VObject circle;
+
+    Minim minim;
+    AudioBuffer ab;
+
+   
    
     public JenniferVisuals(Visual v) {
         super(v);
         this.v = v;
         speaker = new Speaker(v, new PVector(v.width/4, v.height/4));
+        circle = new Circles(v, new PVector(v.width/4, v.height/4));
+
+        minim = new Minim(this);
+        ab = v.audioPlayer().mix;
+
     }
 
     public void render(int elapsed) {
@@ -22,9 +37,31 @@ public class JenniferVisuals extends VScene {
         // 1:03 - 1:48 - Second verse & chorus
         if (elapsed > v.toMs(1, 3, 25) && elapsed < v.toMs(1, 48, 0)) //start at 1:03:something so the beats match up
         {
-            speaker.render();                  
+            //speaker.render();
+            circle.render();                  
         }
         System.out.println(elapsed);
+    }
+
+    class Circles extends VObject {
+        Circles(Visual v, PVector pos){
+            super(v, pos);
+        }
+
+        @Override
+        public void render(){
+            v.background(0);
+            for (int i=0; i<360; i++)
+            {
+                float c = v.map(ab.get(i), -1, 1, 0, 255);
+                v.stroke(c, 255, 255);
+                float f = ab.get(i) * v.height/2;
+                double x2 = v.width/2 + (Math.cos(i)*(Math.PI/180) * 100 * f);
+                double y2 = v.height/2 + (Math.sin(i)*(Math.PI/180) * 100 * f);
+                v.line(v.width/2, v.height/2, (float)x2, (float)y2);
+                
+            }
+        }
     }
 
     class Speaker extends VObject {
@@ -62,9 +99,10 @@ public class JenniferVisuals extends VScene {
             v.rect(x3 - border, y3 - border, width, length);
 
             v.noStroke();
-            v.frameRate((float)0.625); //1/96 of a second bc 96bpm
-            
-            float h = v.random(0, 360);
+            v.frameRate(1); //1/96 of a second bc 96bpm
+           
+            //float h = v.random(0, 360);
+            float h = v.random(ab.get(radius), 360);
             for (int r = radius; r > 0; --r) {
                 v.fill(h, 90, 90);
                 v.ellipse(x1, y1, r, r);
