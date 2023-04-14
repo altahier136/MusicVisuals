19c0cc3c1bd643f874de68b4c30351db973b192a
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-04-14 15:57:49 +0100

    Documented and refactored VAnimation

--- java/src/ie/tudublin/visual/VAnimation.java
+++ java/src/ie/tudublin/visual/VAnimation.java
@@ -6,30 +6,30 @@ import processing.core.PApplet;
 
 /**
  * VAnimation is a class that is used to handle animating a value over time.
- * User creates an instance of VAnimation which adds it to the list of
- * animations in the Visual class. The user adds transition sections with a
- * selected ease function.
- * The class then fills in the gaps between the sections with linear
- * interpolation.
- *
- * Class details with millisecond time units and float values.
+ * Animations are made up of sections. Each section has a duration, start value,
+ * end value and an ease function.
  */
 public class VAnimation {
-    int lengthMs; // Duration of the animation in milliseconds
-    ArrayList<Section> sections;
+    private int lengthMs; // Duration of the animation in milliseconds
+    private ArrayList<Section> sections;
 
+    /**
+     * Constructor for VAnimation
+     *
+     * @param lengthMs Duration of the whole animation in milliseconds
+     */
     public VAnimation(int lengthMs) {
         this.lengthMs = lengthMs;
         sections = new ArrayList<Section>();
     }
 
     /**
-     * Appends a section to the animation
+     * Appends a section to the animation list
      *
-     * @param durationMs
-     * @param startValue
-     * @param endValue
-     * @param easeFunction
+     * @param durationMs   Duration of the section in milliseconds
+     * @param startValue   Start value of the section
+     * @param endValue     End value of the section
+     * @param easeFunction Ease function of the section
      */
     public void appendSection(int durationMs, float startValue, float endValue, EaseFunction easeFunction) {
         // If no sections, set start time to 0
@@ -44,6 +44,15 @@ public class VAnimation {
         sections.add(new Section(startTime, durationMs, startValue, endValue, easeFunction));
     }
 
+    /**
+     * Adds a transition to the animation
+     *
+     * @param startMs      Start time of the transition in milliseconds
+     * @param durationMs   Duration of the transition in milliseconds
+     * @param startValue   Start value of the transition
+     * @param endValue     End value of the transition
+     * @param easeFunction Ease function of the transition
+     */
     public void addTransition(int startMs, int durationMs, float startValue, float endValue,
             EaseFunction easeFunction) {
         // Check if it overlaps with any existing sections
@@ -52,34 +61,35 @@ public class VAnimation {
                 throw new RuntimeException("Transition overlaps with existing section");
         }
 
-        // If right after end of last section,
-        // fill gap with linear transition from end value of last section to start value
-        // of new section
-        // then add transition section
-        if (sections.size() != 0) {
+        if (sections.size() == 0 && startMs != 0) {
+            // If no sections, add linear transition from 0 to start value
+            appendSection(startMs - 1, startValue, startValue, EaseFunction.easeLinear);
+        } else {
+            // If there are sections, add linear transition from end value of last section
             Section lastSection = sections.get(sections.size() - 1);
             if (startMs != lastSection.getEndTime() + 1) {
                 int gapDuration = startMs - lastSection.getEndTime() - 1;
-                appendSection(gapDuration, lastSection.getValue(lastSection.getEndTime()), startValue, EaseFunction.linearEase);
-            }
-        } else {
-            // If no sections, check if start time is 0
-            if (startMs != 0) {
-                appendSection(startMs - 1, startValue, startValue, EaseFunction.linearEase);
+                appendSection(gapDuration, lastSection.getValue(lastSection.getEndTime()), startValue,
+                        EaseFunction.easeLinear);
             }
         }
+        // Add transition section
         appendSection(durationMs, startValue, endValue, easeFunction);
 
     }
 
+    /**
+     * Returns the value of the animation at the given time
+     *
+     * @param time
+     * @return
+     */
     public float getValue(int time) {
-        // Check if time is within animation
         if (time < 0 || time > lengthMs) {
-            // throw new RuntimeException("Time is not within the animation");
             System.out.println("Time is not within the animation");
         }
 
-        // Find the section that the time is in
+        // Search through sections to find the one that contains the time
         for (Section section : sections) {
             if (time >= section.getStartTime() && time < section.getEndTime()) {
                 return section.getValue(time);
@@ -90,8 +100,9 @@ public class VAnimation {
         return 0;
     }
 
-    // Represents a section of the animation which as a start, duration and ease
-    // function
+    /**
+     * Represents a section of the animation
+     */
     class Section {
         private int startTime;
         private int duration;
@@ -99,6 +110,15 @@ public class VAnimation {
         private float endValue;
         private EaseFunction easeFunction;
 
+        /**
+         * Constructor for Section
+         *
+         * @param startTimeMs  Start time of the section in milliseconds
+         * @param durationMs   Duration of the section in milliseconds
+         * @param startValue   Start value of the section
+         * @param endValue     End value of the section
+         * @param easeFunction Ease function of the section
+         */
         public Section(int startTimeMs, int durationMs, float startValue, float endValue, EaseFunction easeFunction) {
             this.startTime = startTimeMs;
             this.duration = durationMs;
@@ -107,17 +127,18 @@ public class VAnimation {
             this.easeFunction = easeFunction;
         }
 
-        /** Returns the start time of the section */
+        /** @return The start time of the section. */
         public int getStartTime() {
             return startTime;
         }
 
+        /** @return The end time of the section. */
         public int getEndTime() {
             return startTime + duration;
         }
 
         /**
-         * Returns the value of the animation at the given time
+         * Returns the value of the animation at the given time.
          *
          * @param time
          * @return
