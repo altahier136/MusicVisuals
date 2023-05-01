951700291a911ded6baff59ded462e1680a592e4
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-05-01 23:46:43 +0100

    Implemented AdriansVisuall::SquigglyArcs

--- java/src/c21348423/AdriansVisual.java
+++ java/src/c21348423/AdriansVisual.java
@@ -1,5 +1,8 @@
 package c21348423;
 
+import com.jogamp.common.nio.Buffers;
+
+import ie.tudublin.visual.AudioAnalysis;
 import ie.tudublin.visual.EaseFunction;
 import ie.tudublin.visual.VAnimation;
 import ie.tudublin.visual.VObject;
@@ -17,6 +20,7 @@ public class AdriansVisual extends VScene {
     VAnimation sceneVisibility;
     Visual v;
     VObject circle;
+    SquigglyArcs squigglyArcs;
     HappyHorse horse;
 
     // Colour pallet
@@ -38,8 +42,9 @@ public class AdriansVisual extends VScene {
 
         circle = new Circle(v, new PVector(0, 0, 0));
         horse = new HappyHorse(v, new PVector(0, 0, 0));
+        squigglyArcs = new SquigglyArcs(v, new PVector(0, 0, 0));
 
-        // v.seek(1, 48);
+        v.seek(1, 48);
         setSceneAnimation();
     }
 
@@ -60,16 +65,32 @@ public class AdriansVisual extends VScene {
             return;
         }
 
-        // v.blendMode(PApplet.BLEND);
-        v.background(0);
-        // v.fill(background, 20);
-        // v.rect(0, 0, v.width, v.height);
-        v.blendMode(PApplet.SUBTRACT);
-        // 1:48 - 2:30 - Instrumental
-        circle.render();
-        v.lights();
         v.blendMode(PApplet.BLEND);
+        // v.background(0);
+        v.fill(background, 20);
+        v.pushMatrix();
+        v.translateCenter();
+        v.translate(0, 0, -1000);
+        v.rectMode(PApplet.CENTER);
+        v.rect(0, 0, v.width * 3, v.height * 3);
+        v.popMatrix();
+
+        // 1:48 - 2:30 - Instrumental
+
+        v.translateCenter();
+        v.rotateX(Visual.sin(-elapsed * BEATMS * Visual.HALF_PI * 0.1f) * Visual.HALF_PI / 2 - Visual.HALF_PI / 2);
+        v.blendMode(PApplet.SUBTRACT);
+        rotation.y = Visual.sin(elapsed * BEATMS * Visual.TWO_PI) * 0.3f;
+        rotation.y = elapsed * BEATMS * 0.3f;
+        v.ambientLight(300, 100, 100);
+        v.pointLight(0, 100, 100, 100, -v.height, 1000);
+        applyTransforms();
+        circle.render();
+        // v.lights();
+        v.blendMode(PApplet.BLEND);
+        squigglyArcs.render(elapsed);
         horse.render(elapsed);
+        v.popMatrix();
     }
 
     class Circle extends VObject {
@@ -83,7 +104,7 @@ public class AdriansVisual extends VScene {
             applyTransforms();
             v.colorMode(PApplet.RGB);
             v.fill(255, 0, 255);
-            v.circle(v.width / 2, v.height / 2, v.audioAnalysis().mix().amplitude * 1000);
+            v.circle(0, 0, v.audioAnalysis().mix().amplitude * 1000);
             v.popMatrix();
         }
     }
@@ -99,14 +120,83 @@ public class AdriansVisual extends VScene {
         @Override
         public void render(int elapsed) {
             applyTransforms();
-            v.translateCenter();
-            v.scale(50);
+            v.scale(40);
 
             // v.rotateZ(Visual.sin(elapsed / 1000f * Visual.TWO_PI) + Visual.PI);
             v.rotateZ(Visual.sin(elapsed * BEATMS * Visual.HALF_PI) / 2 + Visual.PI);
-            v.translate(0, Visual.sin(elapsed * BEATMS * Visual.TWO_PI ) * 2 + 2);
-            v.shape(horse);
+            v.translate(0, Visual.sin(elapsed * BEATMS * Visual.TWO_PI) * 2 + 2);
+            // v.translate(0,200);
+            v.shape(horse, 0, 2);
             v.popMatrix();
         }
+    }
+
+    /**
+     * SquigglyArcs
+     */
+    public class SquigglyArcs extends VObject {
+        private float halfH = v.height / 2;
+        private float halfW = v.width / 2;
+        private AudioAnalysis aa;
+        private float PI;
+        private float TWO_PI;
+
+        public SquigglyArcs(Visual v, PVector position) {
+            super(v, position);
+            aa = v.audioAnalysis();
+            PI = PApplet.PI;
+            TWO_PI = PApplet.TWO_PI;
+        }
+
+        @Override
+        public void render(int elapsed) {
+            v.colorMode(PApplet.HSB, 360, 100, 100, 100);
+            float lerpedAmplitude = aa.mix().lerpedAmplitude;
+            float[] lerpedSpectrum = aa.mix().lerpedSpectrum;
+            // 1000 / 60
+            // float off = 16.6666666667f / elapsed;
+            float off = 1 / elapsed;
+
+            v.pushMatrix();
+            v.rectMode(PApplet.CENTER);
+            // v.translate(0, 100, -100);
+            // rect(0,0, width, height);
+            // v.square(halfW, halfH, v.height * 2);
+            // v.rotateX(PApplet.PI );
+            v.translate(0, 200, 0);
+            v.rotateX(PApplet.HALF_PI);
+            v.scale(2);
+            v.noFill();
+            float sinMap = PApplet.map(PApplet.sin(off * 1.00001f / (60 * 3)), -1, 1, 2.090f, 2.110f);
+            // How many arcs to render
+            int count = (int) PApplet.map(lerpedAmplitude * 2, 0, 0.2f, 0, lerpedSpectrum.length - 1);
+            count += PApplet.constrain((int) PApplet.map(lerpedAmplitude, 0.01f, 0, 0, lerpedSpectrum.length - 1), 0,
+                    lerpedSpectrum.length);
+            v.blendMode(PApplet.BLEND);
+            // count = ab.size();
+            for (int i = 0; i < PApplet.constrain(count, 0, lerpedSpectrum.length) - 1; i++) {
+                float hue = 350 - PApplet.map(i, 0, lerpedSpectrum.length, 0, 360);
+
+                float sat = 100;
+                float val = 100;
+                float alpha = PApplet.map(i, 0, lerpedSpectrum.length, 100, 50) + (lerpedAmplitude * 2 * 100);
+                v.stroke(hue, sat, val, alpha);
+
+                float f = lerpedSpectrum[i] * PI; //
+                f += 0.1f;
+
+                float r = PApplet.sin(i * sinMap) / 1 * PI / 2; // Sine wave rotation
+                r += PApplet.sin(off / (120)) / 1 * PI / 2; // Sine wave rotation
+                r += off / (60); // 1 rotation per second
+
+                // Two arcs
+                v.arc(0, 0, i, i, r - f, r + f);
+                r += PApplet.PI;
+                v.arc(0, 0, i, i, r - f, r + f);
+
+            }
+            v.popMatrix();
+        }
+
     }
 }
