f09a032bd5704317939bd3c25b137a40ff97ab54
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-05-03 00:37:28 +0100

    Began implementing PressStart visual

---
+++ java/src/global/PressStart.java
@@ -0,0 +1,14 @@
+package global;
+
+import ie.tudublin.visual.VScene;
+import ie.tudublin.visual.Visual;
+import processing.core.PVector;
+
+public class PressStart extends VScene {
+
+    public PressStart(Visual v, PVector position) {
+        super(v, position);
+        //TODO Auto-generated constructor stub
+    }
+
+}
